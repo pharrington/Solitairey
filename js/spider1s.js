248d5e523aa909db54724ea98a09453bb17d1d55
Paul Harrington <pharrington@badenoughdu.de>
2010-12-09 23:52:07 -0500

    don't overwrite the Y.Solitaire.Spider.Deck object

--- js/spider1s.js
+++ js/spider1s.js
@@ -1,6 +1,8 @@
 YUI.add("spider1s", function (Y) {
 	var Spider = Y.Solitaire.Spider1S = instance(Y.Solitaire.Spider);
 
-	Spider.Deck.suits = ["s"];
-	Spider.Deck.count = 8;
+	Spider.Deck = instance(Y.Solitaire.Spider.Deck, {
+		suits: ["s"],
+		count: 8
+	});
 }, "0.0.1", {requires: ["spider"]});

c121818be7ca184d14c106922f9faa6d2697bd9d
Paul Harrington <pharrington@badenoughdu.de>
2010-12-10 23:15:17 -0500

    no longer a mixin; call Solitaire.AutoStackClear.register instead

--- js/auto-stack-clear.js
+++ js/auto-stack-clear.js
@@ -4,50 +4,52 @@
 YUI.add("auto-stack-clear", function (Y) {
 	var Solitaire = Y.Solitaire;
 
-	Solitaire.AutoStackClear = {
-		isComplete: function (callback) {
-			var cards = this.cards,
-			    rank,
-			    suit,
-			    card,
-			    complete,
-			    i;
+	Y.namespace("Solitaire.AutoStackClear");
 
-			if (!cards.length) { return false; }
+	Solitaire.AutoStackClear.register = function () {
+		Y.on("solitaire|tableau:afterPush", function (stack) {
+			isComplete(stack, clearComplete);
+		});
+	}
 
-			for (i = cards.length - 1, rank = 1, suit = cards[i].suit; i >= 0 && rank < 14; i--, rank++) {
-				card = cards[i];
+	function isComplete(stack, callback) {
+		var cards = stack.cards,
+		    rank,
+		    suit,
+		    card,
+		    complete,
+		    i;
 
-				if (card.isFaceDown || card.rank !== rank || card.suit !== suit) {
-					return false;
-				}
+		if (!cards.length) { return false; }
+
+		for (i = cards.length - 1, rank = 1, suit = cards[i].suit; i >= 0 && rank < 14; i--, rank++) {
+			card = cards[i];
+
+			if (card.isFaceDown || card.rank !== rank || card.suit !== suit) {
+				return false;
 			}
-
-			complete = rank === 14;
-			complete && typeof callback === "function" && callback.call(this, i + 1);
-			return complete;
-		},
-
-		clearComplete: function (startIndex) {
-			var foundation,
-			    cards = this.cards,
-			    count = cards.length - startIndex;
-
-			// find the first empty foundation
-			foundation = Y.Array.find(Solitaire.game.foundation.stacks, function (stack) {
-				return !stack.cards.length;
-			});
-
-			Solitaire.stationary(function () {
-				while (count) {
-					cards.last().moveTo(foundation);
-					count--;
-				}
-			});
-		},
-
-		afterPush: function () {
-			return !this.isComplete(this.clearComplete);
 		}
-	};
+
+		complete = rank === 14;
+		complete && typeof callback === "function" && callback(stack, i + 1);
+		return complete;
+	}
+
+	function clearComplete(stack, startIndex) {
+		var foundation,
+		    cards = stack.cards,
+		    count = cards.length - startIndex;
+
+		// find the first empty foundation
+		foundation = Y.Array.find(Solitaire.game.foundation.stacks, function (stack) {
+			return !stack.cards.length;
+		});
+
+		Solitaire.stationary(function () {
+			while (count) {
+				cards.last().moveTo(foundation);
+				count--;
+			}
+		});
+	}
 }, "0.0.1", {requires: ["solitaire"]});

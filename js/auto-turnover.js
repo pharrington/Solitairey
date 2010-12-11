bbe5a848fd303f545b90eb3e4753678cdf2e6526
Paul Harrington <pharrington@badenoughdu.de>
2010-12-10 22:19:46 -0500

    use events for more robustness/less duplication

--- js/auto-turnover.js
+++ js/auto-turnover.js
@@ -1,12 +1,10 @@
 /*
- * mixin to automatically turn over the first open faceup card in a stack
+ * automatically turn over the first open faceup card in a stack
  */
 YUI.add("auto-turnover", function (Y) {
-	Y.Solitaire.AutoTurnover = {
-		update: function () {
-			var card = this.cards.last();
+	Y.on("tableau:afterPop", function (stack) {
+		var card = stack.cards.last();
 
-			card && card.isFaceDown && card.faceUp();
-		}
-	};
+		card && card.isFaceDown && card.faceUp();
+	});
 }, "0.0.1", {requires: ["solitaire"]});

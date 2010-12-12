345b8448c5ffd9006caeb3366e4c204a9e9f7105
Paul Harrington <pharrington@badenoughdu.de>
2010-12-12 03:12:55 -0500

    track statistics file

---
+++ js/statistics.js
@@ -0,0 +1,61 @@
+/*
+ * record win/lose records, streaks, etc
+ */
+YUI.add("statistics", function (Y) {
+	var loaded,
+	    localStorage = window.localStorage;
+	    Solitaire = Y.Solitaire;
+
+	Y.on("newGame", function () {
+		if (loaded) { recordLose(); }
+
+		loaded = null;
+	});
+
+	Y.on("loadGame", function () {
+		loaded = Solitaire.game.name();
+	});
+
+	Y.on("endTurn", function () {
+		if (!loaded) {
+			loaded = Solitaire.game.name();
+			saveProgress();
+		}
+	});
+
+	Y.on("win", function () {
+		loaded = null;
+
+		recordWin();
+	});
+
+	function record(value) {
+		var key = Solitaire.game.name() + "record",
+		    record = localStorage[key] || "";
+
+		record += new Date().getTime() + "_" + value + "|";
+
+		localStorage[key] = record;
+	}
+
+	function recordLose() {
+		record(0);
+
+		clearProgress();
+	}
+
+	function recordWin() {
+		record(1);
+
+		clearProgess();
+	}
+
+	function clearProgress() {
+		localStorage.removeItem("currentGame");
+	}
+
+	function saveProgress() {
+		localStorage["currentGame"] = Solitaire.game.name();
+	}
+
+}, "0.0.1", {requires: ["solitaire"]});

d19827ec23da205539b7365c43b6efc1605628b1
Paul Harrington <pharrington@badenoughdu.de>
2010-12-14 04:49:08 -0500

    add statistic parsing

--- js/statistics.js
+++ js/statistics.js
@@ -30,6 +30,55 @@ YUI.add("statistics", function (Y) {
 		recordWin();
 	});
 
+	function Record(raw) {
+		function parse() {
+			var entries = raw.split("|");
+
+			entries.splice(entries.length - 1);
+
+			return Y.Array.map(entries, function (entry) {
+				entry = entry.split("_");
+
+				return {date: new Date(entry[0]), won: !!entry[1]};
+			});
+		}
+
+		function won(entry) {
+			return entry.won;
+		}
+
+		var record = parse();
+
+		return {
+			streaks: function () {
+				var streaks = [],
+				    streak = null;
+
+				Y.Array.each(record, function (entry) {
+					if (!entry.won) {
+						streak && streaks.push(streak);
+						streak = null;
+					} else {
+						if (!streak) { streak = []; }
+						streak.push(entry);
+					}
+				});
+
+				streak && streaks.push(streak);
+
+				return streaks;
+			},
+
+			wins: function () {
+				return Y.Array.filter(record, won);
+			},
+
+			loses: function () {
+				return Y.Array.reject(record, won);
+			}
+		};
+	}
+
 	function record(value) {
 		var key = Solitaire.game.name() + "record",
 		    record = localStorage[key] || "";
@@ -59,4 +108,4 @@ YUI.add("statistics", function (Y) {
 		localStorage["currentGame"] = Solitaire.game.name();
 	}
 
-}, "0.0.1", {requires: ["solitaire"]});
+}, "0.0.1", {requires: ["solitaire", "array-extras"]});

3f305dc7f5e420b4d7fc12c6a978e1d74d9a2677
Paul Harrington <pharrington@badenoughdu.de>
2010-12-15 21:10:37 -0500

    add autoplay

---
+++ js/autoplay.js
@@ -0,0 +1,55 @@
+YUI.add("solitaire-autoplay", function (Y) {
+	var Solitaire = Y.Solitaire,
+	    autoPlayInterval = null,
+	    autoPlayable = ["Klondike", "FortyThieves", "GClock", "Freecell", "FlowerGarden", "Yukon"];
+
+	Y.on("endTurn", function () {
+		if (autoPlayable.indexOf(Solitaire.game.name()) === -1) { return; }
+
+		if (autoPlayInterval === null && isWon()) {
+			autoPlayInterval = setInterval(autoPlay, 500);
+		}
+	});
+
+	Y.on("win", function () {
+		clearInterval(autoPlayInterval);
+		autoPlayInterval = null;
+	});
+
+	function autoPlay() {
+		var played = false;
+
+		Solitaire.game.eachStack(function (stack) {
+			var field = stack.field;
+
+			if (played || field === "foundation" || field === "deck") { return; }
+
+			played = !stack.eachCard(function (card) {
+				return !card.autoPlay();
+			});
+		});
+	}
+
+	function isWon() {
+		var stop = false;
+
+		Solitaire.game.eachStack(function (stack) {
+			var field = stack.field,
+			    prevRank = 14,
+			    decending;
+
+			if (stop || field !== "tableau" && field !== "waste") { return; }
+
+			decending = stack.eachCard(function (card) {
+				if (card.rank >= prevRank) {
+					stop = true;
+					return false;
+				} else {
+					prevRank = card.rank;
+				}
+			});
+		});
+
+		return !stop;
+	}
+}, "0.0.1", {requires: ["solitaire"]});

aa09113939cfb49f375d81d6915ef5623286c7ee
unknown <Paul@.(none)>
2010-12-24 07:23:53 -0500

    fix bugs

--- js/klondike1t.js
+++ js/klondike1t.js
@@ -1,7 +1,7 @@
 YUI.add("klondike1t", function (Y) {
-	var Solitaire = Y.Solitiare,
+	var Solitaire = Y.Solitaire,
 	    Klondike = Solitaire.Klondike,
-	    Klondike1T = instance(Klondike, {
+	    Klondike1T = Solitaire.Klondike1T = instance(Klondike, {
 		redeal: Solitaire.noop,
 
 		turnOver: function () {
@@ -9,7 +9,11 @@ YUI.add("klondike1t", function (Y) {
 			    waste = this.waste.stacks[0],
 			    card = deck.last();
 
-			card && card.moveTo(waste);
+			card && card.faceUp().moveTo(waste);
 		}
-	    }
+	    });
+
+	Klondike1T.Waste.Stack = instance(Klondike.Waste.Stack, {
+		update: Solitaire.noop
+	});
 }, "0.0.1", {requires: ["klondike"]});

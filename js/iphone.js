48ec395232dc9422c06e5ec70fac141c2072dc43
Paul Harrington <pharrington@badenoughdu.de>
2011-01-12 13:31:31 -0500

    set Scorpion offset

--- js/iphone.js
+++ js/iphone.js
@@ -10,6 +10,7 @@ YUI.add("solitaire-ios", function (Y) {
 		"FortyThieves": {offset: 10, scale: 0.9},
 		"MonteCarlo": {scale: 0.78, offset: 80},
 		"Pyramid": {offset: 20},
+		"Scorpion": {offset: 40},
 		"Spider": {scale: 0.95, offset: 10},
 		"Spider1S": {scale: 0.95, offset: 10},
 		"Spider2S": {scale: 0.95, offset: 10}

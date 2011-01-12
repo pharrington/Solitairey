c83d05c82a3391c6568e8cb1f2de7315925960d1
Paul Harrington <pharrington@badenoughdu.de>
2011-01-12 13:26:37 -0500

    set MonteCarlo scale

--- js/iphone.js
+++ js/iphone.js
@@ -8,6 +8,7 @@ YUI.add("solitaire-ios", function (Y) {
 	    	"TriTowers": {scale: 0.90, offset: 10},
 		"Freecell": {offset: 35},
 		"FortyThieves": {offset: 10, scale: 0.9},
+		"MonteCarlo": {scale: 0.78, offset: 80},
 		"Spider": {scale: 0.95, offset: 10},
 		"Spider1S": {scale: 0.95, offset: 10},
 		"Spider2S": {scale: 0.95, offset: 10}

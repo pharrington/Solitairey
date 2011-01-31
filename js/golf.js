01e1847a7270f19811d5a83becb59ba6aa688555
Paul Harrington <pharrington@badenoughdu.de>
2011-01-31 09:57:32 -0500

    add Golf solitaire

---
+++ js/golf.js
@@ -0,0 +1,167 @@
+YUI.add("golf", function (Y) {
+	var Solitaire = Y.Solitaire,
+	Golf = Y.Solitaire.Golf = instance(Solitaire, {
+		fields: ["Deck", "Foundation", "Tableau"],
+
+		width: function () { return this.Card.base.width * 10; },
+		height: function () { return this.Card.base.height * 5; },
+
+		deal: function () {
+			var card,
+			    stack,
+			    stacks = this.tableau.stacks,
+			    deck = this.deck,
+			    foundation = this.foundation.stacks[0],
+			    row;
+
+
+			for (row = 0; row < 5; row++) {
+				for (stack = 0; stack < 7; stack++) {
+					card = deck.pop().faceUp();
+					stacks[stack].push(card);
+				}
+			}
+
+			card = deck.pop().faceUp();
+			foundation.push(card);
+
+			deck.createStack();
+		},
+
+		turnOver: function () {
+			var deck = this.deck.stacks[0],
+			    foundation = this.foundation.stacks[0],
+			    last = deck.last();
+
+			last && last.faceUp().moveTo(foundation);
+		},
+
+		isWon: function () {
+			var won = true;
+
+			this.eachStack(function (stack) {
+				stack.eachCard(function (card) {
+					if (card) { won = false; }
+
+					return won;
+				});
+			}, "tableau");
+
+			return won;
+		},
+
+		Deck: instance(Solitaire.Deck, {
+			field: "deck",
+			stackConfig: {
+				total: 1,
+				layout: {
+					hspacing: 0,
+					top: function () { return Solitaire.Card.height * 3; },
+					left: 0
+				}
+			},
+
+			createStack: function () {
+				var i, len;
+
+				for (i = 0, len = this.cards.length; i < len; i++) {
+					this.stacks[0].push(this.cards[i]);
+				}
+			}
+		}),
+
+		Tableau: {
+			field: "tableau",
+			stackConfig: {
+				total: 7,
+				layout: {
+					hspacing: 1.25,
+					top: 0,
+					left: 0
+				}
+			}
+		},
+
+		Foundation: {
+			field: "foundation",
+			stackConfig: {
+				total: 1,
+				layout: {
+					hspacing: 0,
+					top: function () { return Solitaire.Card.height * 3; },
+					left: function () { return Solitaire.Card.width * 4; }
+				}
+			}
+		},
+
+		Events: instance(Solitaire.Events, {
+			dragCheck: function (e) {
+				this.getCard().autoPlay();
+
+				/* workaround because YUI retains stale drag information if we halt the event :\ */
+				this._afterDragEnd();
+				e.halt();
+			}
+		}),
+
+		Card: instance(Solitaire.Card, {
+			/*
+			 * return true if the target is 1 rank away from the this card
+			 */
+			validTarget: function (stack) {
+				if (stack.field !== "foundation") { return false; }
+
+				var target = stack.last(),
+				    diff = Math.abs(this.rank - target.rank);
+
+				return diff === 1;
+			},
+
+			isFree: function () {
+				return !this.isFaceDown && this === this.stack.last();
+			},
+		}),
+		     
+		Stack: instance(Solitaire.Stack, {
+			images: {}
+		})
+	}, true);
+
+	Y.Array.each(Golf.fields, function (field) {
+		Golf[field].Stack = instance(Golf.Stack);
+	});
+
+	Y.mix(Golf.Tableau.Stack, {
+
+		setCardPosition: function (card) {
+			var last = this.cards.last(),
+			    top = last ? last.top + last.rankHeight : this.top,
+			    left = this.left;
+
+			card.left = left;
+			card.top = top;
+		}
+	}, true);
+
+	Y.mix(Golf.Deck.Stack, {
+		setCardPosition: function (card) {
+			var last = this.last(),
+			    top,
+			    left,
+			    zIndex;
+
+			top = this.top;
+			if (last) {
+				left = last.left + card.width * 0.1;
+				zIndex = last.zIndex + 1;
+			} else {
+				left = this.left;
+				zIndex = 0;
+			}
+
+			card.top = top;
+			card.left = left;
+			card.zIndex = zIndex;
+		}
+	}, true);
+}, "0.0.1", {requires: ["solitaire"]});

57664665fc83be563600248393890fbac4072702
Paul Harrington <pharrington@badenoughdu.de>
2011-01-31 19:42:59 -0500

    add russian solitaire

---
+++ js/russian-solitaire.js
@@ -0,0 +1,26 @@
+YUI.add("russian-solitaire", function (Y) {
+
+  var Solitaire = Y.Solitaire,
+    RussianSolitaire = Solitaire.RussianSolitaire = instance(Solitaire.Yukon);
+
+  RussianSolitaire.Card.validTarget = function (stack) {
+    var target = stack.last();
+
+    switch (stack.field) {
+    case "tableau":
+      if (!target) {
+         return this.rank === 13;
+       } else {
+         return !target.isFaceDown && target.suit !== this.suit && target.rank === this.rank + 1;
+       }
+    case "foundation":
+      if (!target) {
+        return this.rank === 1;
+      } else {
+        return target.suit === this.suit && target.rank === this.rank - 1;
+      }
+    default:
+      return false;
+    }
+  };
+}, "0.0.1", {requires: ["yukon"]);

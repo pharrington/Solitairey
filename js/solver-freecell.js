9e58cd6cb8c90e39d1e5e41acc8d4c35e5424538
Paul Harrington <pharrington@badenoughdu.de>
2011-08-14 19:56:03 -0400

    factor the actual solver out into a web worker

--- js/solver-freecell.js
+++ js/solver-freecell.js
@@ -89,7 +89,7 @@ YUI.add("solver-freecell", function (Y) {
 		var reserve, foundation, tableau;
 
 		tableau = Y.Array.map(sortedStacks(game.tableau), function (s) {
-			var buffer = new Uint8Array(new ArrayBuffer(s.cards.length));
+			var buffer = [];
 
 			s.eachCard(function (c, i) {
 				buffer[i] = cardToValue(c);
@@ -98,538 +98,19 @@ YUI.add("solver-freecell", function (Y) {
 			return [buffer, s.cards.length];
 		});
 
-		reserve = new Uint8Array(new ArrayBuffer(4));
+		reserve = [];
 		Y.Array.forEach(sortedStacks(game.reserve), function (s, i) {
 			reserve[i] = cardToValue(s.last());
 		});
 
-		foundation = new Uint8Array(new ArrayBuffer(4));
+		foundation = [];
 		Y.Array.forEach(sortedStacks(game.foundation), function (s, i) {
 			foundation[i] = cardToValue(s.last());
 		});
 
-
-		return new GameState(reserve, foundation, tableau);
+		return {reserve: reserve, foundation: foundation, tableau: tableau};
 	}
 
-	function GameState(reserve, foundation, tableau) {
-		this.reserve = reserve;
-		this.foundation = foundation;
-		this.tableau = tableau;
-	}
-
-	GameState.fromState = function (other) {
-		var state = new GameState();
-
-		state.tableau = other.tableau;
-		state.reserve = other.reserve;
-		state.foundation = other.foundation;
-
-		return state;
-	}
-
-	GameState.prototype = {
-		reserve: null,
-		foundation: null,
-		tableau: null,
-		rating: null,
-		parentMove: null,
-		parent: null,
-		child: null,
-
-		solved: function () {
-			var i, len,
-			    foundation = this.foundation;
-
-			for (i = 0, len = 4; i < len; i++) {
-				if ((foundation[i] >> 2) !== 13) { return false; }
-			}
-
-			return true;
-		},
-
-		eachTableau: function (callback) {
-			var i, len,
-			    stack,
-			    tableau = this.tableau;
-
-			for (i = 0, len = tableau.length; i < len; i++) {
-				stack = tableau[i];
-				callback.call(this, stack[0], stack[1], i);
-			}
-		},
-
-		validTarget: function (field, value, start) {
-			var rank = value >> 2,
-			    suit = value & 3,
-			    dest,
-			    tableau,
-			    i, len;
-		
-			if (!value) { return -1; }
-
-			if (start === undefined) {
-				start = 0;
-			} else {
-				start++;
-			}
-
-			switch (field) {
-			case "foundation":
-				for (i = 0; i < 4; i++ ) {
-					dest = this.foundation[i];
-
-					if ((!dest && rank === 1) ||
-					    (suit === (dest & 3) &&
-					    rank === (dest >> 2) + 1)) {
-						return i;
-					}
-				}
-				break;
-
-			case "reserve":
-				for (i = 0; i < 4; i++) {
-					if (!this.reserve[i]) {
-						return i;
-					}
-				}
-				break;
-
-			case "tableau":
-				tableau = this.tableau;
-
-				for (i = start, len = tableau.length; i < len; i++) {
-					dest = tableau[i][0][tableau[i][1] - 1];
-
-					if (!tableau[i][1] ||
-					    (((suit & 1) ^ (dest & 1)) &&
-					    (rank === (dest >> 2) - 1))) {
-						return i;
-					}
-				}
-				break;
-			}
-
-			return -1;
-		},
-
-		move: function (sourceField, sourceStack, destField, destStack) {
-			var val = this.pop(sourceField, sourceStack);
-			this.push(destField, destStack, val);
-		},
-
-		pop: function (field, stack) {
-			var val,
-			    newBuffer,
-			    bufferLength,
-			    tableau,
-			    i, len;
-
-			if (field === "reserve" || field === "foundation") {
-				val = this[field][stack];
-
-				this[field] = new Uint8Array(this[field]);
-				this[field][stack] = 0;
-				return val;
-			}
-
-			tableau = this.tableau;
-			bufferLength = tableau[stack][1];
-
-			if (!bufferLength) { return 0; }
-
-			val = tableau[stack][0][bufferLength - 1];
-			this.copyTableau(stack, bufferLength - 1);
-			return val;
-		},
-
-		push: function (field, stack, val) {
-			var newLength;
-			if (!val) { return; }
-
-			if (field === "reserve" || field === "foundation") {
-				this[field] = new Uint8Array(this[field]);
-				this[field][stack] = val;
-				return;
-			}
-
-			newLength = this.tableau[stack][1] + 1;
-			this.copyTableau(stack, newLength);
-			this.tableau[stack][0][newLength - 1] = val;
-
-		},
-
-		copyTableau: function (stack, newLength) {
-			var old = this.tableau,
-			    tableau = old[stack][0],
-			    newBuffer = new Uint8Array(new ArrayBuffer(newLength)),
-			    i, len;
-
-			for (i = 0; i < newLength; ++i) {
-				newBuffer[i] = tableau[i];
-			}
-
-			this.tableau = [];
-
-			for (i = 0, len = old.length; i < len; ++i) {
-				if (i !== stack) {
-					this.tableau[i] = old[i];
-				} else {
-					this.tableau[i] = [newBuffer, newLength];
-				}
-			}
-		},
-
-		sort: function () {
-			Array.prototype.sort.call(this.reserve);
-			Array.prototype.sort.call(this.foundation);
-			this.tableau.sort(function (a, b) {
-				return a[0][0] - b[0][0];
-			});
-		},
-
-		_serialized: null,
-		// TODO write a serialize function
-		serialize: function () {
-			if (this._serialized !== null) { return this._serialized; }
-
-			var i, j, len, stack;
-
-			this._serialized = "";
-			for (i = 0; i < 4; i++) {
-				this._serialized += String.fromCharCode(this.reserve[i]);
-			}
-
-			this._serialized += "_";
-
-			for (i = 0; i < 4; i++) {
-				this._serialized += String.fromCharCode(this.foundation[i]);
-			}
-
-			this._serialized += "_";
-
-			for (i = 0; i < 8; i++) {
-				stack = this.tableau[i];
-
-				for (j = 0; j < stack[1]; j++) {
-					this._serialized += String.fromCharCode(stack[0][j]) + "_";
-				}
-			}
-
-			return this._serialized;
-		},
-
-		rateMove: function (sourceField, sourceIndex, destField, destIndex) {
-			var RATING_FOUNDATION = 1000,
-			    RATING_CLOSEDTABLEAUFOLLOWUP = 20,
-			    RATING_FREEFOUNDATIONTARGET = 15,
-			    RATING_OPENTABLEAU = 15,
-			    RATING_FREETABLEAUTARGET = 10,
-			    RATING_OPENRESERVE = 10,
-			    RATING_TABLEAU = 2,
-			    RATING_RESERVE = -1,
-			    RATING_BURYFOUNDATIONTARGET = -5,
-			    RATING_CLOSEDTABLEAU = -10,
-			rating = 0,
-			stack,
-			card,
-			nextCard,
-			followup = false,
-			i, length;
-
-			// reward moves to the foundation
-			if (destField === "foundation") {
-				rating += RATING_FOUNDATION;
-			}
-
-			if (sourceField === "tableau") {
-				stack = this.tableau[sourceIndex];
-				length = stack[1];
-				card = stack[0][length - 1];
-
-				// reward an opened tableau slot
-				if (length === 1) {
-					rating += RATING_OPENTABLEAU;
-				}
-
-				// reward unburing foundation targets
-				for (i = length - 2; i >= 0; i--) {
-					if (this.validTarget("foundation", stack[0][i]) > -1) {
-						rating += RATING_FREEFOUNDATIONTARGET - (length - 2 - i) + (13 - (stack[0][i] >> 2));
-					}
-				}
-
-				// reward a newly discovered tableau-to-tableau move
-				if (this.validTarget("tableau", stack[0][length - 2]) > -1) {
-					rating += RATING_FREETABLEAUTARGET;
-				}
-			}
-
-			// reward an opened reserve slot
-			if (sourceField === "reserve") {
-				rating += RATING_OPENRESERVE;
-				card = this.reserve[sourceIndex];
-			}
-			// reward any move to the tableau
-			if (destField === "tableau") {
-				rating += RATING_TABLEAU;
-
-				stack = this.tableau[destIndex];
-				length = stack[1];
-				// punish a move to the tableau that buries a foundation target
-				for (i = length - 1; i >= 0; i--) {
-					if (this.validTarget("foundation", stack[0][i]) > -1) {
-						rating += RATING_BURYFOUNDATIONTARGET * (length - i);
-					}
-				}
-	
-
-				if (stack[1] === 0) {
-					// reward a move to an empty stack that can be followed up be another move
-					for (i = 0; i < 4; i++) {
-						nextCard = this.reserve[i];
-						if (((nextCard >> 2) === (card >> 2) - 1) &&
-						    ((nextCard & 1) ^ (card & 1))) {
-							rating += RATING_CLOSEDTABLEAUFOLLOWUP;
-							followup = true;
-						}
-					}
-
-					for (i = 0; i < 8; i++) {
-						stack = this.tableau[i];
-						nextCard = stack[0][stack[1] - 1];
-						if (((nextCard >> 2) === (card >> 2) - 1) &&
-						    ((nextCard & 1) ^ (card & 1))) {
-							rating += RATING_CLOSEDTABLEAUFOLLOWUP;
-							followup = true;
-						}
-					}
-
-					// punish filling a tableau slot with no immediate followup
-					if (!followup) {
-						rating += RATING_CLOSEDTABLEAU;
-					}
-				}
-			}
-
-			// penalize moving to the reserve
-			if (destField === "reserve") {
-				rating += RATING_RESERVE;
-			}
-			return rating;
-		},
-
-		transformParentMove: function () {
-			var move = this.parentMove,
-			    parent = this.parent;
-
-			if (!(move && parent)) { return; }
-
-			move.source[1] = parent.lastCard(move.source[0], move.source[1]);
-			move.dest[1] = parent.lastCard(move.dest[0], move.dest[1]);
-		},
-
-		lastCard: function (field, index) {
-			var stack, length;
-
-			switch (field) {
-			case "reserve":
-			case "foundation":
-				return this[field][index];
-
-			case "tableau":
-				stack = this[field][index];
-				length = stack[1];
-
-				return stack[0][length - 1];
-			}
-		},
-
-		becomeChild: function () {
-			var parent = this.parent;
-
-			if (!parent) { return; }
-
-			parent.child = this;
-			this.transformParentMove();
-		}
-	};
-
-	function Tree(value) {
-		this.value = value;
-		this.children = [];
-	}
-
-	Tree.prototype = {
-		children: null,
-		parent: null,
-
-		addChildren: function (children) {
-			children.forEach(function (c) {
-				c.parent = this;
-				this.children.push(c);
-			}, this);
-		},
-
-		addChild: function (child) {
-			child.parent = this;
-			this.children.push(child);
-		},
-
-		findParent: function (compare) {
-			var p = this;
-
-			while (p = p.parent) {
-				if (compare.equals(p.value)) { return p; }
-			}
-
-			return null;
-		},
-
-		findChild: function (compare) {
-			if (compare.equals(this.value)) { return this; }
-
-			var i, len, children = this.children;
-
-			for (i = 0, len = children.length; i < len; i++) {
-				if (children[i].findChild(compare)) { return children[i]; }
-			}
-
-			return null;
-		}
-	};
-
-	// returns the depth of tree to jump up to, or 0 if the solution is found
-	function solve(state, depth, visited, movesSinceFoundation, fastSearch) {
-		var jumpDepth,
-		    maxDepth = 200,
-		    sourceIndex, destIndex, length, val,
-		    next, sourceField, destField,
-		    tableau,
-		    move, moves = [],
-		    scale = 1,
-		    foundFoundation = false,
-		    i;
-
-		/*
-		 * if the state is the solved board, return
-		 * for each reserve and tableau stack, find all valid moves
-		 * for each valid move, create a new game state
-		 * sort each state by rank, add for each thats undiscoverd, and it as a branch and recurse
-		 * stop iterating if a child state is solved
-		 */
-
-		if (depth > maxDepth) { return maxDepth; }
-
-		/* when the state is solved
-		 * replace the stack index with its associated move with the actual card that was moved
-		 * set the parent states child to this state
-		 * then jump out of the tree
-		 */
-		if (state.solved()) {
-			state.becomeChild();
-			return 0;
-		}
-
-		// find moves from the reserve
-		for (i = 0; i < 4; i++) {
-			val = state.reserve[i];
-
-			if (!val) { continue; }
-
-			destIndex = state.validTarget("foundation", val);
-			if (destIndex > -1) {
-				moves.push({source: ["reserve", i], dest: ["foundation", destIndex]});
-				foundFoundation = true;
-			}
-
-			if (foundFoundation) { break; }
-
-			destIndex = 0;
-			while ((destIndex = state.validTarget("tableau", val, destIndex)) > -1) {
-				moves.push({source: ["reserve", i], dest: ["tableau", destIndex]});
-			}
-		}
-
-		// find moves from the tableau
-		tableau = state.tableau;
-		for (i = 0; i < tableau.length; i++) {
-			s = tableau[i][0];
-			length = tableau[i][1];
-			val = s[length - 1];
-
-			if (!val) { continue; }
-
-			destIndex = state.validTarget("foundation", val);
-			if (destIndex > -1) {
-				moves.push({source: ["tableau", i], dest: ["foundation", destIndex]});
-				foundFoundation = true;
-			}
-
-			if (foundFoundation) { break; }
-
-			destIndex = state.validTarget("reserve", val);
-			if (destIndex > -1) {
-				moves.push({source: ["tableau", i], dest: ["reserve", destIndex]});
-			}
-
-			destIndex = 0;
-			while ((destIndex = state.validTarget("tableau", val, destIndex)) > -1) {
-				moves.push({source: ["tableau", i], dest: ["tableau", destIndex]});
-			}
-		}
-
-		if (foundFoundation) {
-			movesSinceFoundation = 0;
-		} else {
-			movesSinceFoundation++;
-		}
-
-		for (i = 0; i < moves.length; i++) {
-			move = moves[i];
-			next = GameState.fromState(state);
-
-			sourceField = move.source[0];
-			sourceIndex = move.source[1];
-
-			destField = move.dest[0];
-			destIndex = move.dest[1];
-
-			next.rating = next.rateMove(sourceField, sourceIndex, destField, destIndex);
-			next.move(sourceField, sourceIndex, destField, destIndex);
-			next.parentMove = move;
-			next.parent = state;
-
-			moves[i] = next;
-		};
-
-		moves.sort(function (a, b) {
-			return b.rating - a.rating;
-		});
-
-		// if nothing's been moved to the foundation in many turns, backtrack alot of steps
-		if (movesSinceFoundation >= 20) {
-			scale = 0.7;
-		}
-
-		for (i = 0; i < moves.length && scale === 1; i++) {
-			move = moves[i];
-			if (jumpDepth < depth) { break; }
-			if (visited[move.serialize()]) {
-				if (fastSearch) { break } else { continue };
-			}
-
-			visited[move.serialize()] = true;
-			jumpDepth = solve(move, depth + 1, visited, movesSinceFoundation, fastSearch);
-		}
-
-		if (jumpDepth === 0) {
-			state.becomeChild();
-		}
-
-		if (jumpDepth === undefined) { jumpDepth = Math.ceil(depth * scale); }
-		return jumpDepth;
-	}
 
 	function moveToCardAndStack(game, move) {
 		var source = move.source,
@@ -668,32 +149,39 @@ YUI.add("solver-freecell", function (Y) {
 		return ret;
 	}
 
-	function animateMove(game, state) {
-		var child = state.child,
-		    move;
+	function animateMove(game, moves) {
+		var move;
 
-		//game.Card.animSpeeds.fast = game.Card.animSpeeds.mid = 0.5;
-		if (!child) { return; }
+		if (!moves) { return; }
 
-		if (child.parentMove) {
-			move = moveToCardAndStack(game, child.parentMove);
-			move.card.moveTo(move.stack);
-		}
+		move = moveToCardAndStack(game, moves);
+		move.card.moveTo(move.stack);
 
-		window.setTimeout(animateMove.partial(game, child), 500);
+		window.setTimeout(animateMove.partial(game, moves.next), 500);
 	}
+
+	function showMenu() {
+		var bar = Y.Node.create("<div id=solver_bar>Solving</div>"),
+		    controls = Y.Node.create(
+			"<div class=controls><div class=play></div><div class=pause></div><div class=rewind></div><div class=fastforward></div></div>");
+
+		bar.append(controls);
+		Y.one("body").append(bar);
+	}
+
+	showMenu();
 
 	Y.mix(FreecellSolver, {
 		test: function () {
-			var a = gameToState(Game);
+			var worker = new Worker("js/solver-freecell-worker.js");
 
-			solve(a, 1, {}, 0, true);
-
-			animateMove(Game, a);
-			return a;
+			worker.postMessage({action: "solve", param: gameToState(Game)});
+			worker.onmessage = function (e) {
+				var data = e.data
+				if (data.solution) {
+					animateMove(Game, data.solution);
+				}
+			};
 		}
 	});
 }, "0.0.1", {requires: ["solitaire"]});
-
-function last(t, index) {
-}

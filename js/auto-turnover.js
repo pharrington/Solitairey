b929e2609efb8cce1117a13ae120341b6db546f2
Paul Harrington <pharrington@badenoughdu.de>
2011-10-11 23:52:53 -0400

    add ability to enable and disabled card autoturnover

--- js/auto-turnover.js
+++ js/auto-turnover.js
@@ -2,11 +2,28 @@
  * automatically turn over the first open faceup card in a stack
  */
 YUI.add("auto-turnover", function (Y) {
+	Y.namespace("Y.Solitaire.AutoTurnover");
+
+	var Turnover = Y.Solitaire.AutoTurnover,
+	    enabled = true;
+
 	Y.on("tableau:afterPop", function (stack) {
+		if (!enabled) { return; }
+
 		Y.Array.each(stack.cards, function (card) {
 			if (card && card.isFaceDown && card.isFree()) {
 				card.faceUp();
 			}
 		});
 	});
+
+	Y.mix(Turnover, {
+		enable: function () {
+			enabled = true;
+		},
+
+		disable: function () {
+			enabled = false;
+		}
+	}, true);
 }, "0.0.1", {requires: ["solitaire"]});

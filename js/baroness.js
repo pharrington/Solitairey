014bede46b0549f6eb6e1e01f8f41369395c2743
Paul Harrington <pharrington@badenoughdu.de>
2011-10-20 19:00:14 -0400

    start adding Baroness

---
+++ js/baroness.js
@@ -0,0 +1,111 @@
+YUI.add("baroness", function (Y) {
+	var Solitaire = Y.Solitaire,
+	    AcesUp = Solitaire.AcesUp,
+	    Baroness = Solitaire.Baroness = instance(AcesUp, {
+
+		createEvents: function () {
+			Solitaire.createEvents.call(this);
+			Y.on("solitaire|endTurn", this.fillTableau.bind(this));
+		},
+
+		turnOver: function () {
+			if (Y.Array.every(this.tableau.stacks, function (stack) {
+				return stack.last();
+			})) {
+				AcesUp.turnOver.call(this);
+			}
+		},
+
+		fillTableau: function () {
+			var tableau = this.tableau.stacks,
+			    deck = this.deck.stacks[0],
+	    		    cardsToFill,
+			    totalCards = Y.Array.reduce(tableau, 0, function (total, stack) {
+				return total + stack.cards.length;
+			});
+
+			if (totalCards >= tableau.length) { return; }
+
+			cardsToFill = 5 - totalCards;
+			this.eachStack(function (stack) {
+				if (!(cardsToFill && deck.cards.length)) { return; }
+
+				if (!stack.last()) {
+					deck.last().faceUp().moveTo(stack);
+					cardsToFill--;
+				}
+			}, "tableau");
+		},
+
+		isWon: Solitaire.isWon,
+
+		Events: instance(Solitaire.Pyramid.Events, {
+			drop: function (e) {
+				var active = Solitaire.activeCard,
+				    foundation = Solitaire.game.foundation.stacks[0],
+				    target = e.drop.get("node").getData("target");
+
+				if (!active) { return; }
+
+				if (target.field) {
+					if (!target.last()) {
+						active.moveTo(target);
+					} else {
+						Solitaire.stationary(function () {
+							target.last().moveTo(foundation);
+							active.moveTo(foundation);
+						});
+					}
+				} else {
+					Solitaire.stationary(function () {
+						target.moveTo(foundation);
+						active.moveTo(foundation);
+					});
+				}
+
+				Y.fire("endTurn");
+			}
+		}),
+
+		Tableau: instance(AcesUp.Tableau, {
+			stackConfig: instance(AcesUp.Tableau.stackConfig, {
+				total: 5
+			})
+		}),
+
+		Stack: instance(AcesUp.Stack, {
+			updateDragGroups: Y.Solitaire.Pyramid.Stack.updateDragGroups
+		}),
+
+		Foundation: instance(AcesUp.Foundation, {
+			stackConfig: {
+				total: 1,
+				layout: {
+					spacing: 0,
+					top: 0,
+					left: function () { return Y.Solitaire.Card.width * 8.5; }
+				}
+			},
+		}),
+
+		Card: instance(AcesUp.Card, {
+			playable: function () {
+				return this.isFree();
+			},
+
+			validTarget: function (target) {
+				var card = target.last();
+
+				if (target.field === "foundation") {
+					return this.isFree() && this.rank === 13;
+				}
+
+				if (!card) {
+					return true;
+				}
+
+				return !card.isFaceDown && (this.rank + card.rank === 13);
+			}
+		})
+	});
+}, "1.0.0", {requires: ["acesup", "pyramid"]});

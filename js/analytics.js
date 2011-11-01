5f657f4285f126807de137fca6e5e0bc7dc833d6
Paul Harrington <pharrington@badenoughdu.de>
2011-10-31 22:07:26 -0400

    track total game play time

--- js/analytics.js
+++ js/analytics.js
@@ -5,19 +5,31 @@ var Solitaire = Y.Solitaire,
     /* minimum number of moves for a new game to be considered started */
     minMoves = 5,
     totalMoves = 0,
+    previousGame,
     start = 0;
 
 if (typeof _gaq === "undefined") { return; }
 
 Y.on("beforeSetup", function () {
-	totalMoves = 0;
+	var end = start;
+
 	start = new Date().getTime();
+
+	if (end) {
+		Analytics.track("Games", "Played", previousGame, end - start);
+	}
+
+	totalMoves = 0;
+	previousGame = Solitaire.game.name();
 });
 
 Y.on("win", function () {
 	var now = new Date().getTime();
 
 	Analytics.track("Games", "Won", Solitaire.game.name(), now - start, true);
+	Analytics.track("Games", "Played", Solitaire.game.name(), now - start, true);
+
+	start = 0;
 });
 
 Y.on("endTurn", function () {

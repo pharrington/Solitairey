19f2e86a27e6b507ad18f948983c6748ffd2368c
Paul Harrington <paul.c.harrington@gmail.com>
2013-03-28 23:46:37 -0400

    WinDisplay: add oldschool windows solitaire win screen

--- js/win-display.js
+++ js/win-display.js
@@ -11,6 +11,7 @@ YUI.add("win-display", function (Y) {
 	    isAttached = false,
 	    cacheNode = Solitaire.Util.cacheNode,
 
+	    bodyNode = cacheNode("body"),
 	    winDisplayNode = cacheNode("#win-display"),
 	    winDisplayGame = cacheNode("#win-display-game"),
 	    winDisplayStreak = cacheNode("#win-display-streak"),
@@ -30,13 +31,170 @@ YUI.add("win-display", function (Y) {
 
 		won = true;
 
-		explodeFoundations();
+		windows3();
 	});
 
 	Y.on("beforeSetup", function () {
 		winDisplayNode().addClass("hidden");
 		WinDisplay.enable();
+		Bouncer.clear();
 	});
+
+	Y.on("fieldResize", function () {
+		Bouncer.resize();
+	});
+
+	var requestAnimationFrame = (function () {
+		var rate = 16;
+
+		return window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function (callback) {
+				setTimeout(callback, rate);
+			}
+	})();
+
+	var Bouncer = {
+		width: 0,
+		height: 0,
+		angle: 2 * Math.PI,
+		velocity: 1000,
+		gravity: 15,
+		dampening: 0.5,
+		canvas: null,
+		context: null,
+		actors: [],
+
+		bounce: function (card, angleRandomFactor, velocityRandomFactor) {
+			var node = card.node,
+				xy = node.getXY(),
+				vector = {},
+				angle, angleMin, angleDelta,
+				velocity, velocityMin, velocityDelta,
+				now = new Date().getTime(),
+				start;
+
+
+			angleMin = this.angle * (1 - angleRandomFactor);
+			angleDelta = this.angle - angleMin;
+			angle = Math.random() * angleDelta + angleMin;
+
+			velocityMin = this.velocity * (1 - velocityRandomFactor);
+			velocityDelta = this.velocity - velocityMin;
+			velocity = Math.random() * velocityDelta + velocityMin;
+
+			vector.x = Math.cos(angle) * velocity;
+			vector.y = Math.sin(angle) * velocity;
+
+			node.remove();
+
+			start = this.actors.length === 0;
+			this.actors.push({
+				node: node._node,
+				velocity: vector,
+				boundingbox: {x: xy[0], y: xy[1], width: ~~card.width, height: ~~card.height},
+				lastUpdate: now,
+				lastSmear: now
+			});
+
+			if (start) {
+				this.bounceCallback(now);
+			}
+		},
+
+		bounceCallback: function (lastUpdate) {
+			var now = new Date().getTime(),
+				dt,
+				actors = this.actors,
+				actor,
+				i;
+
+			dt = now - lastUpdate;
+
+			i = 0;
+			while (actor = actors[i]) {
+				if (this.bounceStep(actor.node, actor.velocity, actor.boundingbox, dt)) {
+					actors.splice(i, 1);
+				} else {
+					i++;
+				}
+				
+			}
+
+			if (actors.length > 0) {
+				requestAnimationFrame(function () {
+					this.bounceCallback(now);
+				}.bind(this));
+			}
+		},
+
+		bounceStep: function (image, velocity, boundingbox, dt) {
+			dt /= 1000;
+
+			boundingbox.x += velocity.x * dt;
+			boundingbox.y += velocity.y * dt;
+
+			velocity.y += this.gravity;
+
+			if (boundingbox.x > this.width || boundingbox.x + boundingbox.width < 0 ||
+				boundingbox.y + boundingbox.height < 0) {
+				return true;
+			}
+
+			if (boundingbox.y + boundingbox.height >= this.height) {
+				boundingbox.y -= velocity.y * dt;
+				velocity.y *= -this.dampening;
+			}
+
+			this.context.drawImage(image, ~~boundingbox.x, ~~boundingbox.y, boundingbox.width, boundingbox.height);
+		},
+
+		createSmearNode: function () {
+			var node;
+
+			if (!this.canvas) {
+				var node = document.createElement("canvas");
+				node.style.zIndex = -1;
+				node.style.position = "absolute";
+				node.style.top = "0px";
+				node.style.left = "0px";
+				node.width = this.width;
+				node.height = this.height;
+
+				this.canvas = node;
+				this.context = node.getContext("2d");
+			}
+
+			bodyNode().appendChild(this.canvas);
+		},
+
+		resize: function () {
+			this.width = bodyNode().get("winWidth");
+			this.height = bodyNode().get("winHeight");
+
+			if (this.canvas) {
+				this.canvas.width = this.width;
+				this.canvas.height = this.height;
+			}
+		},
+
+		init: function () {
+			this.resize();
+			this.createSmearNode();
+		},
+
+		clear: function () {
+			if (!this.context) { return; }
+
+			this.context.clearRect(0, 0, this.width, this.height);
+			bodyNode().removeChild(this.canvas);
+
+			this.actors = [];
+		}
+	};
 
 	function attachEvents() {
 		if (isAttached) { return; }
@@ -53,6 +211,26 @@ YUI.add("win-display", function (Y) {
 		}, Y.one("#win-display .choose_game"));
 
 		isAttached = true;
+	}
+
+	function windows3() {
+		var delay = 300,
+			interval = 1000;
+
+		Bouncer.init();
+
+		Game.eachStack(function (stack) {
+			stack.eachCard(function (card, index) {
+				card.node.setStyle("zIndex", -2);
+				setTimeout(function () {
+					Bouncer.bounce(card, 0.8, 0.2);
+				}, ~~(interval * (stack.cards.length - 1 - index) + Math.random() * interval + delay));
+			});
+		}, "foundation");
+
+		setTimeout(function () {
+			WinDisplay.winDisplay();
+		}, delay + 10000);
 	}
 
 	function explodeFoundations() {
